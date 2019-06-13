e8444b761b820587d876e0213b4887707599768d
minhquang <vumhquangg@gmail.com>
2019-06-13 16:15:32 +0700

    13/06/2019**

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
-end
+    def full_title(page_title = '')
+        base_title = "Shino APPPPPPPPPPPP"
+        if page_title.empty?
+            base_title
+        else
+            page_title + " | " + base_title    
+        end
+    end
+    end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'static_pages#home'
-  get 'static_pages/about'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get  'static_pages/about'
+  get  '/about',   to: 'static_pages#about'
+  get  '/home',    to: 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/signup',  to: 'users#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
