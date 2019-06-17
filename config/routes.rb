6979c3e9d6bbfbf96e005bd868a943e9c8fc0dfa
minhquang <vumhquangg@gmail.com>
2019-06-17 14:26:42 +0700

    17/06/2019*

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
   get  '/home',    to: 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/signup',  to: 'users#new'
   post  '/signup', to: 'users#create'
+  delete '/logout',  to: 'sessions#destroy'
+  get '/login',   to: 'sessions#new'
+  post  '/login', to: 'sessions#create'
   resources :users
 end
   
