6c7e5ff5d42368f13b4cabc053dc358cbb4302bc
minhquang <vumhquangg@gmail.com>
2019-06-17 15:17:17 +0700

    17/06/2019**

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,6 +9,7 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
+//= require jquery
 //= require bootstrap
 //= require rails-ujs
 //= require activestorage
